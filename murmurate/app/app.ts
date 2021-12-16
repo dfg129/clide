863bbd75c39cab120d1061609d408ba771d35866
dfg129 <dfg129@gmail.com>
2021-12-15 21:59:44 -0500

    containerize this puppy

---
+++ murmurate/app/app.ts
@@ -0,0 +1,7 @@
+
+
+exports.handler = async function(event, context) {
+//	console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+	console.log("###### to here ")
+	return "narnia" //context.logStreamName
+}
--- murmurate/lib/murmurate-stack.ts
+++ murmurate/lib/murmurate-stack.ts
@@ -1,14 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
-import { aws_lambda as lambda } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as path from "path";
 
 export class MurmurateStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'MurmurationBucket', {
+    const bucket = new s3.Bucket(this, 'MurmurationBucket', {
 	    versioned: false,
 	    removalPolicy: cdk.RemovalPolicy.DESTROY,
 	    autoDeleteObjects: true,
@@ -16,8 +17,12 @@ export class MurmurateStack extends cdk.Stack {
     });
 
 
-    new lambda.DockerImageFunction(this, 'EventDrop', {
-	    code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, 'app'),
+    const fn = new lambda.DockerImageFunction(this, 'EventDrop', {
+	    code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '..')),
     });
+
+    fn.addEventSource(new S3EventSource(bucket, {
+	    events: [ s3.EventType.OBJECT_CREATED, s3.EventType.OBJECT_REMOVED ],
+    }));
   }
 }
